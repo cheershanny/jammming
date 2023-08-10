d868052701677a51865d531d170c1f86eebcd87b
Hanny Tran <cheershanny@gmail.com>
2023-08-10 16:56:11 +0100

    delete default md file

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>hi my lovely dearing Han</h2>
+        <h2>hi my lovely dearing Hannnnny</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
